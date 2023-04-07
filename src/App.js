17df31b68877bb894cd42842672dbc01940f7118
DanoHwang <danohwang00@gmail.com>
2023-04-07 13:34:35 +0900

    Feat: Create Side Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Header from './Pages/Header';
+import Side from './Pages/Side';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="main">
+        <Side />
+      </div>
     </div>
   );
 }
---
+++ src/Pages/Side.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Side.css';
+
+const Side = () => {
+  return (
+    <div className="header__title">
+      My<br/>Agora<br/>States
+    </div>
+  );
+};
+
+export default Side;
